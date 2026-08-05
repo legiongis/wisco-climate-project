5364ae127bee48a05bb8b9a67fb33d79570ea566
Adam Cox <mr.adamcox@gmail.com>
2026-08-05 09:24:56 -0500

    implement footer note, move neighborhood note up

--- types.ts
+++ types.ts
@@ -24,6 +24,7 @@ export interface MarkdownStory {
     coords: [number, number];
     videoUrl?: string;
     htmlContent: string;
+    footerHtml?: string;
 }
 
 export type StoryProperties = StoryKnownProperties & GeoJsonProperties;
