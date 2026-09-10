643e7c8ed9f289680bdd8f1e1cfeb0ef09cfe8d5
Adam Cox <mr.adamcox@gmail.com>
2026-09-10 09:45:07 -0500

    update attribution

--- components/Map.tsx
+++ components/Map.tsx
@@ -82,7 +82,7 @@ const MapComponent: React.FC<MapComponentProps> = ({
 
                 // Use a clean, modern tile layer
                 L.tileLayer("https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png", {
-                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>',
+                    attribution: '&copy; <a href="https://stadiamaps.com/">StadiaMaps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap Contributors</a>',
                     maxZoom: 19,
                 }).addTo(currentLeafletMap);
 
